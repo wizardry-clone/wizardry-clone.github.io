8c95091c8656c57fa16f77df0d936e919969b0ff
wizardry-clone <74430347+wizardry-clone@users.noreply.github.com>
2020-11-15 12:49:10 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "images/logo.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "CocaColaRP";
+var l_serverName = "Wizardry Clone Wars Roleplay";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
